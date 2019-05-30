4d15bb6bce87390de760ed756daf0bcd50f86780
minhly <minhly@kms-technology.com>
2019-05-30 13:02:44 +0700

    Init app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { MlkmsCommonUiModule } from 'mlkms-common-ui';
 
 import { AppRoutingModule } from './app-routing.module';
+import { AppBootstrapModule } from './app-bootstrap.module';
+
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
+import { HeroDetailComponent } from './components/hero-detail/hero-detail.component';
+import { LoginPageComponent } from './components/login-page/login-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroesComponent,
+    HeroDetailComponent,
+    LoginPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    CommonModule,
+    MlkmsCommonUiModule,
+    AppBootstrapModule
   ],
   providers: [],
   bootstrap: [AppComponent]
